fe243c3135ff7ed89656f8d9aeef617afbab849b
wyl <yulu.w@qq.com>
2024-10-24 14:02:23 +0800

    fix help

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func convertEnglishToChinese(word string) {
 
 func main() {
 	var eWord string
-	flag.StringVar(&eWord, "e", "", "Input English Word or sentence")
+	flag.StringVar(&eWord, "e", "", "Input an English word or sentence")
 
 	flag.Parse()
 	if eWord != "" {
